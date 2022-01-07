8a4a4076a95650a00f21673f874ab9aa1b6be263
tree <tree@hidden.cz>
2022-01-07 09:36:03 +0100

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,15 +5,22 @@
 <script>
   import SvelteTooltip from '$lib/SvelteTooltip.svelte'
   import Avatar from '$lib/Avatar.svelte'
+  import SvelteMarkdown from 'svelte-markdown'
+  import { page } from '$app/stores';
 
   import staticBundle from '$lib/bundle.json'
 
   let bundle = staticBundle
 
-  const liveBundleLoader = (async () => {
-		const response = await fetch('https://spec.utxo.cz/22/bundle.json')
-    bundle = await response.json()
-	})()
+  let hostname = $page.url.hostname
+  if (hostname === 'localhost') {
+    console.log('Local only mode')
+  } else {
+    const liveBundleLoader = (async () => {
+      const response = await fetch('https://spec.utxo.cz/22/bundle.json')
+      bundle = await response.json()
+    })()
+  }
 
   const origName = 'UTXO.22'
   let h1 = origName
@@ -54,7 +61,7 @@
         <div class="mt-3 mb-6 flex flex-wrap justify-center relative">
         {#each bundle.spec.speakers as speaker}
           {#if speaker.lead}
-            <div on:mouseover={() => mouseOverSpeaker(speaker)} on:mouseleave={mouseLeaveSpeaker}>
+            <div class="rounded-full" on:mouseover={() => mouseOverSpeaker(speaker)} on:mouseleave={mouseLeaveSpeaker}>
               <Avatar speaker={speaker} lead={true} category={category} visible={!selectedSpeaker || (selectedSpeaker && selectedSpeaker.id === speaker.id)} />
             </div>
           {/if}
@@ -83,13 +90,27 @@
 
     <div class="subline-shadow2 text-lg md:text-xl lg:text-2xl mt-14"><div>2 dny, 50+ přednášejích, 100h+ obsahu, 1000+ návštěvníků</div></div>
 
+    <h2 class="pixelfont mt-10 text-gray-800">Často kladené dotazy (FAQ)</h2>
 
-    <div class="flex flex-wrap justify-center relative mt-10">
-      <a class="m-2" href={bundle.links.docs}><i class="fas fa-book"></i> Dokumentace</a>
-      <a class="m-2" href={bundle.links.twitter}><i class="fab fa-twitter"></i> Twitter</a>
-      <a class="m-2" href={bundle.links.discord}><i class="fab fa-discord"></i> Discord</a>
-      <a class="m-2" href={bundle.links.telegram}><i class="fab fa-telegram-plane"></i> Telegram</a>
-      <a class="m-2" href={bundle.links.fbevent}><i class="fab fa-facebook"></i> Facebook</a>
+    <div class="container w-full">
+      <div class="columns-1 xl:columns-2 mt-5 h-auto text-gray-800">
+        {#each bundle.spec.faqs as item}
+          <div class="container mb-5 break-inside-avoid-column bg-white/30 rounded-3xl px-6 py-4 text-left transition-all">
+            <h3 class="pixelfont text-sm mb-3">{item.question}</h3>
+            <p class="md">
+              <SvelteMarkdown source={item.answer} />
+            </p>
+          </div>
+        {/each}
+      </div>
+    </div>
+
+    <div class="flex flex-wrap justify-center relative mt-5 pixelfont text-sm">
+      <a class="m-3" href={bundle.links.docs}><i class="fas fa-book"></i> Dokumentace</a>
+      <a class="m-3" href={bundle.links.twitter}><i class="fab fa-twitter"></i> Twitter</a>
+      <a class="m-3" href={bundle.links.discord}><i class="fab fa-discord"></i> Discord</a>
+      <a class="m-3" href={bundle.links.telegram}><i class="fab fa-telegram-plane"></i> Telegram</a>
+      <a class="m-3" href={bundle.links.fbevent}><i class="fab fa-facebook"></i> Facebook</a>
     </div>
 
   </div>
@@ -132,5 +153,7 @@
     color: white;
     text-shadow: 0.2em 0.2em #00000040;
   }
+
+  .md blockquote { margin-top: 1em; }
 
 </style>
