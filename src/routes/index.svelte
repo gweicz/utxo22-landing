28772cd13cb4e5fee2cd12f6ac9cd7452ea19355
tree <tree@hidden.cz>
2022-01-07 07:27:53 +0100

    Update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,14 +3,17 @@
 </script>
 
 <script>
-  import SvelteTooltip from '$lib/SvelteTooltip.svelte';
-  import Avatar from '$lib/Avatar.svelte';
+  import SvelteTooltip from '$lib/SvelteTooltip.svelte'
+  import Avatar from '$lib/Avatar.svelte'
 
-  const bundle = (async () => {
+  import staticBundle from '$lib/bundle.json'
+
+  let bundle = staticBundle
+
+  const liveBundleLoader = (async () => {
 		const response = await fetch('https://spec.utxo.cz/22/bundle.json')
-    return await response.json()
+    bundle = await response.json()
 	})()
-
 
   const origName = 'UTXO.22'
   let h1 = origName
@@ -37,22 +40,19 @@
 </script>
 
 <svelte:head>
-	<title>UTXO.22 | Otevřená komunitní kryptoměnová konference</title>
+  <title>{bundle.name} | {bundle.description}</title>
 </svelte:head>
 
 <section>
   <div class="relative px-6 pt-10 pb-8 sm:max-w-6xl sm:mx-auto sm:rounded-lg sm:px-5 text-center">
     <h1 class="text-5xl md:text-6xl lg:text-7xl">{h1}</h1>
-    <div class="subline-shadow text-lg md:text-xl lg:text-2xl">4.-5. červen 2022 @ Gabriel Loci, Praha</div>
-    <div class="subline text-sm md:text-md lg:text-lg text-gray-800">Otevřená komunitní kryptoměnová konference</div>
+    <div class="subline-shadow text-lg md:text-xl lg:text-2xl">4.-5. červen 2022 @ {bundle.place}</div>
+    <div class="subline text-sm md:text-md lg:text-lg text-gray-800">{bundle.description}</div>
 
     <div class="mt-10">
-      {#await bundle}
-        <div style="min-height: 450px;">Načítám obsah ...</div>
-      {:then data}
-        <!--h2 class="pixelfont md:text-md lg:text-lg">Přednášející ({data.spec.speakers.length})</h2-->
+        <!--h2 class="pixelfont md:text-md lg:text-lg">Přednášející ({bundle.spec.speakers.length})</h2-->
         <div class="mt-3 mb-6 flex flex-wrap justify-center relative">
-        {#each data.spec.speakers as speaker}
+        {#each bundle.spec.speakers as speaker}
           {#if speaker.lead}
             <div on:mouseover={() => mouseOverSpeaker(speaker)} on:mouseleave={mouseLeaveSpeaker}>
               <Avatar speaker={speaker} lead={true} category={category} visible={!selectedSpeaker || (selectedSpeaker && selectedSpeaker.id === speaker.id)} />
@@ -63,7 +63,7 @@
 
         <!--h2 class="pixelfont">Další přednášející</h2-->
         <div class="flex flex-wrap justify-center relative">
-        {#each data.spec.speakers as speaker}
+        {#each bundle.spec.speakers as speaker}
           {#if !speaker.lead}
             <div on:mouseover={() => mouseOverSpeaker(speaker)} on:mouseleave={mouseLeaveSpeaker}>
               <Avatar speaker={speaker} category={category} visible={!selectedSpeaker || (selectedSpeaker && selectedSpeaker.id === speaker.id)} />
@@ -74,23 +74,22 @@
         <div class="pixelfont text-sm mt-5 mb-10">.. a další přednášející připravujeme!</div>
 
         <div class="mt-12 mb-6 flex flex-wrap justify-center relative gap-4 text-sm">
-          {#each data.spec.tracks as track}
+          {#each bundle.spec.tracks as track}
             <div on:mouseover={() => mouseOverCat(track.id)} on:mouseleave={mouseLeaveCat}
               class="block transition-all pixelfont text-xs rounded-3xl w-auto bg-white/20 hover:bg-white/60 text-gray-800 px-6 py-4 cursor-pointer {selectedSpeaker && !selectedSpeaker.tracks?.includes(track.id) ? 'opacity-20' : ''}">{track.shortname || track.name}</div>
           {/each}
         </div>
-      {/await}
     </div>
 
     <div class="subline-shadow2 text-lg md:text-xl lg:text-2xl mt-14"><div>2 dny, 50+ přednášejích, 100h+ obsahu, 1000+ návštěvníků</div></div>
 
 
     <div class="flex flex-wrap justify-center relative mt-10">
-      <a class="m-2" href="https://docs.utxo.cz"><i class="fas fa-book"></i> Dokumentace</a>
-      <a class="m-2" href="https://twitter.com/utxocz"><i class="fab fa-twitter"></i> Twitter</a>
-      <a class="m-2" href="https://discord.gg/5k9dEtVhnv"><i class="fab fa-discord"></i> Discord</a>
-      <a class="m-2" href="https://t.me/utxocz"><i class="fab fa-telegram-plane"></i> Telegram</a>
-      <a class="m-2" href="https://www.facebook.com/events/276727151106692"><i class="fab fa-facebook"></i> Facebook</a>
+      <a class="m-2" href={bundle.links.docs}><i class="fas fa-book"></i> Dokumentace</a>
+      <a class="m-2" href={bundle.links.twitter}><i class="fab fa-twitter"></i> Twitter</a>
+      <a class="m-2" href={bundle.links.discord}><i class="fab fa-discord"></i> Discord</a>
+      <a class="m-2" href={bundle.links.telegram}><i class="fab fa-telegram-plane"></i> Telegram</a>
+      <a class="m-2" href={bundle.links.fbevent}><i class="fab fa-facebook"></i> Facebook</a>
     </div>
 
   </div>
@@ -134,19 +133,4 @@
     text-shadow: 0.2em 0.2em #00000040;
   }
 
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
